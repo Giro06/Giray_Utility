cd8df16395d039b79d84bc71420099c5941d4dd7
Giro06 <girayiilhana3@gmail.com>
2021-08-23 21:53:36 +0300

    Timer added.

---
+++ Timer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Giroo.Utility
+{
+    public struct Timer
+    {
+        private float targetTime;
+        private float currentTime;
+        public bool isDone => targetTime <= currentTime;
+
+        public float SettedTime => targetTime;
+        public float PassedTime => currentTime;
+        public float NormalizedTime => currentTime / targetTime;
+
+
+        private bool isHaveAction;
+        private Action actionToDo;
+
+        private bool timeCircleDone;
+
+        public Timer(float time)
+        {
+            this.targetTime = time;
+            this.currentTime = 0;
+            isHaveAction = false;
+            actionToDo = null;
+            timeCircleDone = false;
+        }
+
+        public Timer(float time, Action action)
+        {
+            this.targetTime = time;
+            this.currentTime = 0;
+            this.isHaveAction = true;
+            this.actionToDo = action;
+            timeCircleDone = false;
+        }
+
+        public void Update(float deltaTime)
+        {
+            if (timeCircleDone) return;
+            
+            currentTime += deltaTime;
+            
+            if (currentTime > targetTime)
+            {
+                currentTime = targetTime;
+                timeCircleDone = true;
+                if (isHaveAction)
+                {
+                    actionToDo?.Invoke();
+                }
+            }
+        }
+
+        public void Restart()
+        {
+            currentTime = 0;
+            timeCircleDone = false;
+        }
+
+        public void ResetTime(float time)
+        {
+            this.targetTime = time;
+            Restart();
+        }
+    }
+}
